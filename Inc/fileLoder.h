5e4637254af99ea0ced8de7bfbcf032652d96e58
Horldsence <m18511047688@163.com>
2024-06-03 13:11:02 +0800

    menu

--- Inc/fileLoder.h
+++ Inc/fileLoder.h
@@ -3,10 +3,10 @@
 
 #include "main.h"
 
-void Load(USER user[],  int *nptr);
-void readFile(char *DATA_FILE);
-void Save(char *FILE_NAME, USER user[],  int n);
-void createFile(char *FILE_NAME);
-void lodeFileFromPath(char *FILE_NAME);
+void Load(USER user[],  int *nptr); // 账户文件读取
+void readFile(char *DATA_FILE); // 学生文件读取
+void Save(char *FILE_NAME, USER user[],  int n); // 账户文件保存
+void createFile(char *FILE_NAME); // 创建数据文件
+void lodeFileFromPath(char *FILE_NAME); // 从路径加载文件
 
 #endif
