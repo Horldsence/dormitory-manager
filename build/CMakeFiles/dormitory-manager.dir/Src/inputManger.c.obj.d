2253db207b0508256b1b48371ea8d62e54929c0b
Horldsence <m18511047688@163.com>
2024-06-08 14:48:37 +0800

    new

---
+++ build/CMakeFiles/dormitory-manager.dir/Src/inputManger.c.obj.d
@@ -0,0 +1,2 @@
+CMakeFiles/dormitory-manager.dir/Src/inputManger.c.obj: \
+ C:\Users\PengHeng\Documents\C_C++\dormitory\ manager\Src\inputManger.c
---
+++ cmake-build-debug-mingw/CMakeFiles/dormitory-manager.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/PengHeng/Documents/C_C++/dormitory manager/Src/accountManager.c" "CMakeFiles/dormitory-manager.dir/Src/accountManager.c.obj" "gcc" "CMakeFiles/dormitory-manager.dir/Src/accountManager.c.obj.d"
+  "C:/Users/PengHeng/Documents/C_C++/dormitory manager/Src/changeInfo.c" "CMakeFiles/dormitory-manager.dir/Src/changeInfo.c.obj" "gcc" "CMakeFiles/dormitory-manager.dir/Src/changeInfo.c.obj.d"
+  "C:/Users/PengHeng/Documents/C_C++/dormitory manager/Src/fileLoder.c" "CMakeFiles/dormitory-manager.dir/Src/fileLoder.c.obj" "gcc" "CMakeFiles/dormitory-manager.dir/Src/fileLoder.c.obj.d"
+  "C:/Users/PengHeng/Documents/C_C++/dormitory manager/Src/inputManger.c" "CMakeFiles/dormitory-manager.dir/Src/inputManger.c.obj" "gcc" "CMakeFiles/dormitory-manager.dir/Src/inputManger.c.obj.d"
+  "C:/Users/PengHeng/Documents/C_C++/dormitory manager/Src/main.c" "CMakeFiles/dormitory-manager.dir/Src/main.c.obj" "gcc" "CMakeFiles/dormitory-manager.dir/Src/main.c.obj.d"
+  "C:/Users/PengHeng/Documents/C_C++/dormitory manager/Src/ouputManager.c" "CMakeFiles/dormitory-manager.dir/Src/ouputManager.c.obj" "gcc" "CMakeFiles/dormitory-manager.dir/Src/ouputManager.c.obj.d"
+  "C:/Users/PengHeng/Documents/C_C++/dormitory manager/Src/studentDelet.c" "CMakeFiles/dormitory-manager.dir/Src/studentDelet.c.obj" "gcc" "CMakeFiles/dormitory-manager.dir/Src/studentDelet.c.obj.d"
+  "C:/Users/PengHeng/Documents/C_C++/dormitory manager/Src/studentFinder.c" "CMakeFiles/dormitory-manager.dir/Src/studentFinder.c.obj" "gcc" "CMakeFiles/dormitory-manager.dir/Src/studentFinder.c.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
